c3bd88db3c23343ff13521f0c1394c04d26b272f
Denis Nekrylov <neodenit@gmail.com>
2021-08-30 23:06:19 +0300

    Fix escaped newline characters handling

--- Neodenit.DialogAssistant.Services/GPT3Service.cs
+++ Neodenit.DialogAssistant.Services/GPT3Service.cs
@@ -20,9 +20,16 @@ namespace Neodenit.DialogAssistant.Services
 
         public async Task<string> GetCompletion(string request)
         {
-            var prompt = settings.PromptStart + request + settings.PromptEnd;
+            static string FixNewLines(string s) => s.Replace("\\n", "\n");
+
+            var promptStart = FixNewLines(settings.PromptStart);
+            var promptEnd = FixNewLines(settings.PromptEnd);
+            var stopSequences = settings.StopSequences.Select(s => FixNewLines(s)).ToArray();
+
+            var prompt = promptStart + request + promptEnd;
+
             var api = new OpenAIAPI(APIAuthentication.LoadFromEnv(), new Engine(settings.Engine));
-            CompletionResult completionResult = await api.Completions.CreateCompletionAsync(prompt, settings.MaxTokens, settings.Temperature, stopSequences: settings.StopSequences);
+            CompletionResult completionResult = await api.Completions.CreateCompletionAsync(prompt, settings.MaxTokens, settings.Temperature, stopSequences: stopSequences);
             var completion = completionResult.Completions.First();
             var completionText = completion.Text;
 
